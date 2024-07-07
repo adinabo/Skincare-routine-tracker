4d44cfcc887d67f75368009f90d03a9c20d02f39
Adina <151134389+adinabo@users.noreply.github.com>
2024-07-07 09:31:39 +0100

    updated routes.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,10 +1,7 @@
-import os
-from taskmanager import app
+from flask import render_template
+from taskmanager import app, db
 
 
-if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
-    )
+@app.route("/")
+def home():
+    return render_template("index.html")
