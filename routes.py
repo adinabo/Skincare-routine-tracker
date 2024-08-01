ba829564050029b466c5ef30ad7616ef960abf7b
Adina <151134389+adinabo@users.noreply.github.com>
2024-08-01 16:57:43 +0100

    edited routes.py

---
+++ routes.py
@@ -0,0 +1,25 @@
+from flask import render_template, url_for, flash, redirect, request
+from app import app, db, bcrypt
+from forms import RegistrationForm, LoginForm
+from models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
