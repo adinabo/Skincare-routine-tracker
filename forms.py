745ec75663b5404b90f256c87fd76636d9100f76
Adina <151134389+adinabo@users.noreply.github.com>
2024-08-01 18:00:39 +0100

    added routes.py

---
+++ forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import User
+
