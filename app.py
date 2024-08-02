f70bf5305e835c182ab1aed79015a090a7487304
Adina <151134389+adinabo@users.noreply.github.com>
2024-08-02 07:35:44 +0100

    added ip and port values

--- app.py
+++ app.py
@@ -16,11 +16,18 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 
+# Import routes
+import routes
 
-from routes import *
+# Run the app
 if __name__ == "__main__":
+    # Use default values for IP and PORT if not set in the environment
+    host = os.environ.get("IP", "127.0.0.1")
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("DEBUG", "False").lower() in ['true', '1', 't']
+
     app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        host=host,
+        port=port,
+        debug=debug
     )
